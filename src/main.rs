fa4a2b838c136be3cd23219664f571340532598a
gustavo <gbuschle@hotmail.com>
2015-08-30 17:48:26 +0200

    colorful numbers

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 extern crate sdl;
 
 use sdl::video::{SurfaceFlag, VideoFlag, Color};
-use sdl::event::{Event, Key, Mouse};
+use sdl::event::{Event, Key};
 
 fn init(screen_width: isize, screen_height: isize) -> sdl::video::Surface {
     sdl::init(&[sdl::InitFlag::Video]);
@@ -17,14 +17,82 @@ fn init(screen_width: isize, screen_height: isize) -> sdl::video::Surface {
     screen
 }
 
+fn draw_square(x: i16, y: i16, w: u16, (r,g,b): (u8, u8, u8), screen: &sdl::video::Surface) {
+    screen.fill_rect(Some(sdl::Rect {x: x, y: y, w: w, h: w}), Color::RGB(r, g, b));
+}
+
+fn draw_num(n: u8, x: i16, y: i16, size: u16, screen: &sdl::video::Surface) {
+    let sub = (size-1) as i16/3;
+    let pack = (size as i16-sub*3)as i16/2;
+    let color = if n == 1 {
+        (100,100,255)
+    }else if n == 2 {
+        (0,255,0)
+    }else if n == 3 {
+        (255,0,0)
+    }else if n == 4 {
+        (0,0,255)
+    }else if n == 5 {
+        (200,50,0)
+    }else if n == 6 {
+        (0,255,255)
+    }else if n == 7 {
+        (255,255,255)
+    }else {
+        (150,150,150)
+    };
+    
+    loop {
+        if n == 1{
+            draw_square(sub+x+pack, sub+y+pack, sub as u16-1, color, screen);
+        }else if n == 2{
+            draw_square(x+pack, y+pack, sub as u16-1,  (255,0, 0), screen);
+            draw_square(sub*2+x+pack, sub*2+y+pack, sub as u16-1, color, screen);
+        }else if n == 3{
+            draw_num(1, x, y, size, screen);
+            draw_num(2, x, y, size, screen);
+        }else if n == 4{
+            draw_num(2, x, y, size, screen);
+            draw_square(x+pack, sub*2+y+pack, sub as u16-1, color, screen);
+            draw_square(sub*2+x+pack, y+pack, sub as u16-1, color, screen);
+        }else if n == 5{
+            draw_num(4, x, y, size, screen);
+            draw_num(1, x, y, size, screen);
+        }else if n == 6{
+            draw_num(4, x, y, size, screen);
+            draw_square(x+pack, sub+y+pack, sub as u16-1, color, screen);
+            draw_square(sub*2+x+pack, sub+y+pack, sub as u16-1, color, screen);
+        }else if n == 7{
+            draw_num(1, x, y, size, screen);
+            draw_num(6, x, y, size, screen);
+        }else if n == 8{
+            draw_num(6, x, y, size, screen);
+            draw_square(sub+x+pack, y+pack, sub as u16-1, color, screen);
+            draw_square(sub+x+pack, sub*2+y+pack, sub as u16-1, color, screen);
+        }
+        break;
+    }
+}
+
 fn draw_field(ref field: &Vec<Vec<bool>>, length: i16, screen: &sdl::video::Surface) {
     let mut n = 0;
     for ref i in field.iter() {
         let mut m = 0;
         for ref j in i.iter() {
-            screen.fill_rect(Some(
-                sdl::Rect {x: m as i16*length as i16, y: n as i16*length as i16, w: length as u16, h: length as u16}
-            ), if **j {Color::RGB(0, 0, 0)} else {Color::RGB(255, 255, 255)});
+            draw_square(m*length+1,
+                      n*length+1,
+                      (length-1) as u16,
+                      if **j {(0, 0, 0)} else {(255, 255, 255)},
+                      screen
+            );
+            if !**j{
+                draw_num(
+                    count_field(m as usize, n as usize, field),
+                    m as i16*length as i16+1,
+                    n as i16*length as i16+1,
+                    (length-1) as u16,
+                    screen);
+            }
             m += 1;
         }
         n += 1;
@@ -37,6 +105,21 @@ fn flip_field(ref mut field: &mut Vec<Vec<bool>>, x: i16, y: i16, size: i16) {
     field[i][j] = !(field[i][j]);
 }
 
+fn count_field(x: usize, y: usize, field: &Vec<Vec<bool>>) -> u8 {
+    let w = field[0].len();
+    let h = field.len();
+    let mut n = 0;
+    for i in 0..3 {
+        if y+i == 0 || y+i == h+1 {continue;}
+        for j in 0..3 {
+            if i == 1 && j == 1 {continue;}
+            if x+j == 0 || x+j == w+1 {continue;}
+            if field[y+i-1][x+j-1] {n += 1}
+        }
+    }
+    n
+}
+
 fn main() {
     const WIDTH: usize = 40;
     const HEIGHT: usize = 30;
@@ -44,17 +127,14 @@ fn main() {
 
     let field = &mut vec![vec![false; WIDTH]; HEIGHT];
 
-    let screen = init((SIZE*WIDTH) as isize, (SIZE*HEIGHT) as isize);
+    let screen = init((SIZE*WIDTH) as isize + 1, (SIZE*HEIGHT) as isize + 1);
     loop {
         match sdl::event::poll_event() {
             Event::Quit => break,
             Event::MouseButton(_, down, mx, my) => {
                 if down {
                     flip_field(field, my as i16, mx as i16, SIZE as i16);
-                    for ref i in field.iter(){
-                        println!("{:?}", i);
-                    }
-                    println!("");
+                    println!("{}", count_field((mx/SIZE as u16) as usize, (my/SIZE as u16) as usize, field));
                 }
             },
             Event::Key(k, _, _, _)
